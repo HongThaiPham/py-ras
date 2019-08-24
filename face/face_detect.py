c4b0fce971d536a31e8f93bd2e14533d9ea68345
Leo Pham <your_email@example.com>
2019-08-24 11:45:59 +0700

    test pi

--- face/face_detect.py
+++ face/face_detect.py
@@ -7,6 +7,6 @@ face_detector = cv2.CascadeClassifier(HAAR_PATH)
 
 
 def face_detect(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, 1.3, 5)
     return faces
